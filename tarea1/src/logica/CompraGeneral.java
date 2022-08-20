43d747b0b8e8e2977d08efbcb3829758b753d950
Santiago Veiga <jconnor@fing.edu.uy>
2022-08-20 15:43:56 -0300

    un par de cambios en CompraGeneral

--- tarea1/src/logica/CompraGeneral.java
+++ tarea1/src/logica/CompraGeneral.java
@@ -5,14 +5,21 @@ public class CompraGeneral{
 	
 	private int cantidad;
 	private Date fecha;
-	private int costo;
+	private int costo; 
 	private Salida salida;
 	
-	public CompraGeneral(int cant, Date f, int costo, Salida salida) {
+	public CompraGeneral(int cant, Date f, int costo, Salida salida) { 
     	this.cantidad = cant;
     	this.fecha = f;
-    	this.costo = costo;
+		this.costo = salida.getActividad().getCosto()*cant ;
     	this.salida = salida;
+
+
+		/*El costo de una CompraGeneral se calcula com
+		o el costo de la Actividad asociada a la instancia
+		 de Salida correspondiente por el atributo cantidad 
+		 de CompraGeneral y se le sustrae el porcentaje dado por descuento de Paquete si corresponde.*/
+
 	}
 	
 	/* Getters */
