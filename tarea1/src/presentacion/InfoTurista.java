d31bbb21730dbc9de465e862169c221cd961200b
Franco Lepratti Gilles <francolepratti@gmail.com>
2022-08-20 15:57:44 -0300

    GUI de listar Usuario, info turista e info Proveedor

---
+++ tarea1/src/presentacion/InfoTurista.java
@@ -0,0 +1,212 @@
+package presentacion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
+public class InfoTurista extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InfoTurista frame = new InfoTurista();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InfoTurista() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Informacion Turista");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridwidth = 3;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 5;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("NickName :");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 1;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 4;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 4;
+		gbc_textField.gridy = 1;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Nombre :");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblNewLabel_1_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_1.gridx = 1;
+		gbc_lblNewLabel_1_1.gridy = 2;
+		contentPane.add(lblNewLabel_1_1, gbc_lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.gridwidth = 4;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 4;
+		gbc_textField_1.gridy = 2;
+		contentPane.add(textField_1, gbc_textField_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Apellido :");
+		GridBagConstraints gbc_lblNewLabel_1_2 = new GridBagConstraints();
+		gbc_lblNewLabel_1_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_2.gridx = 1;
+		gbc_lblNewLabel_1_2.gridy = 3;
+		contentPane.add(lblNewLabel_1_2, gbc_lblNewLabel_1_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.gridwidth = 4;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 4;
+		gbc_textField_2.gridy = 3;
+		contentPane.add(textField_2, gbc_textField_2);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("Email :");
+		GridBagConstraints gbc_lblNewLabel_1_3 = new GridBagConstraints();
+		gbc_lblNewLabel_1_3.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_3.gridx = 1;
+		gbc_lblNewLabel_1_3.gridy = 4;
+		contentPane.add(lblNewLabel_1_3, gbc_lblNewLabel_1_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.gridwidth = 4;
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 4;
+		gbc_textField_3.gridy = 4;
+		contentPane.add(textField_3, gbc_textField_3);
+		
+		JLabel lblNewLabel_1_3_1 = new JLabel("FechaDeNacimiento :");
+		GridBagConstraints gbc_lblNewLabel_1_3_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1_3_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_3_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_3_1.gridx = 1;
+		gbc_lblNewLabel_1_3_1.gridy = 5;
+		contentPane.add(lblNewLabel_1_3_1, gbc_lblNewLabel_1_3_1);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.gridwidth = 4;
+		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 4;
+		gbc_textField_4.gridy = 5;
+		contentPane.add(textField_4, gbc_textField_4);
+		
+		JLabel lblNewLabel_1_3_2 = new JLabel("Nacionalidad :");
+		GridBagConstraints gbc_lblNewLabel_1_3_2 = new GridBagConstraints();
+		gbc_lblNewLabel_1_3_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_3_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_3_2.gridx = 1;
+		gbc_lblNewLabel_1_3_2.gridy = 6;
+		contentPane.add(lblNewLabel_1_3_2, gbc_lblNewLabel_1_3_2);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.gridwidth = 4;
+		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 4;
+		gbc_textField_5.gridy = 6;
+		contentPane.add(textField_5, gbc_textField_5);
+		
+		JLabel lblNewLabel_1_3_2_1 = new JLabel("Salidas :");
+		GridBagConstraints gbc_lblNewLabel_1_3_2_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1_3_2_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_3_2_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_3_2_1.gridx = 1;
+		gbc_lblNewLabel_1_3_2_1.gridy = 7;
+		contentPane.add(lblNewLabel_1_3_2_1, gbc_lblNewLabel_1_3_2_1);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.gridwidth = 4;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 5;
+		gbc_comboBox.gridy = 7;
+		contentPane.add(comboBox, gbc_comboBox);
+		
+		JButton btnNewButton = new JButton("Info. Salida");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 6;
+		gbc_btnNewButton.gridy = 8;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Salir");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_1.gridx = 9;
+		gbc_btnNewButton_1.gridy = 9;
+		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
+	}
+
+}
---
+++ tarea1/src/presentacion/ListarUsuarios.java
@@ -0,0 +1,76 @@
+package presentacion;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ListarUsuarios extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ListarUsuarios dialog = new ListarUsuarios();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ListarUsuarios() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JComboBox comboBox = new JComboBox();
+			contentPanel.add(comboBox, BorderLayout.CENTER);
+		}
+		{
+			JLabel lblNewLabel = new JLabel("Selecione Un Usuario");
+			contentPanel.add(lblNewLabel, BorderLayout.NORTH);
+		}
+		{
+			JButton btnNewButton = new JButton("Informacion del usuario");
+			btnNewButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			contentPanel.add(btnNewButton, BorderLayout.SOUTH);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
