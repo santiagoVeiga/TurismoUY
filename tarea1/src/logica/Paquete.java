87ceaee96ffb040856b6040e9eae887772e285cc
Franco Lepratti Gilles <francolepratti@gmail.com>
2022-08-26 20:10:45 -0300

    implementado constructor Paquete

--- tarea1/src/logica/Paquete.java
+++ tarea1/src/logica/Paquete.java
@@ -1,10 +1,27 @@
 package logica;
 
+import java.util.Date;
+
 public class Paquete {
 
+	private String N;
+	private String D;
+	private String desc;
+	private Date F;
+	private String V;
+
+	public Paquete(String N,String D,String desc,Date F,String V)
+	{
+		this.N = N;
+		this.D = D;
+		this.desc = desc;
+		this.F = F;
+		this.V = V;
+	}
+	
 	public DataPaquete getDataP()
 	{
-		return new DataPaquete();
+		return new DataPaquete(N, D, desc, F, V);
 	}
 	
 }
