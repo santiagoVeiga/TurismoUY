6b5bcbeba147e48d5b50a537fff62d24bfadc108
Tobias Iroa Otero <tobias.iroa@fing.edu.uy>
2022-09-15 15:42:24 -0300

    act registro

--- tarea2/WebContent/js/main.js
+++ tarea2/WebContent/js/main.js
@@ -264,24 +264,53 @@
 
 })(jQuery);
 
-function init() {
-  
-    var _inputs = document.getElementsByTagName('input');
-
-    for (var i = 0; i < _inputs.length; i++) {
-
-        if(!Modernizr.inputtypes[_inputs[i].type]) {
-            _inputs[i].className = 'not-supported';
-        }
-
-        _inputs[i].parentNode.getElementsByClassName('result')[0].innerHTML = _inputs[i].value;
-
-        _inputs[i].onchange = function () {
-            //console.log(this.value);
-            var result_node = this.parentNode.getElementsByClassName('result');
-            result_node[0].innerHTML = this.value;
-        }
-    }
-}
-
-window.onload = init;
+$().ready(function() {
+		// validate signup form on keyup and submit
+		$("#registro").validate({
+			rules: {
+				firstname: "required",
+				lastname: "required",
+				username: {
+					required: true,
+					minlength: 2
+				},
+				password: {
+					required: true,
+					minlength: 5
+				},
+				confirm_password: {
+					required: true,
+					minlength: 5,
+					equalTo: "#password"
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				topic: {
+					required: "#newsletter:checked",
+					minlength: 2
+				},
+				agree: "required"
+			},
+			messages: {
+				firstname: "Please enter your firstname",
+				lastname: "Please enter your lastname",
+				username: {
+					required: "Please enter a username",
+					minlength: "Your username must consist of at least 2 characters"
+				},
+				password: {
+					required: "Please provide a password",
+					minlength: "Your password must be at least 5 characters long"
+				},
+				confirm_password: {
+					required: "Please provide a password",
+					minlength: "Your password must be at least 5 characters long",
+					equalTo: "Please enter the same password as above"
+				},
+				email: "Please enter a valid email address",
+				agree: "Please accept our policy",
+				topic: "Please select at least 2 topics"
+			}
+		}); });
