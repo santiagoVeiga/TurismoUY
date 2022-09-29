b760fc3e83fbd9f6b4db78be0cecfab6565d4210
Agustin <agustin.emanuel.de.leon@fing.edu.uy>
2022-09-29 16:29:11 -0300

    ObtenerPaquetesCompradosTurista

--- tarea1/src/logica/IControladorInsc.java
+++ tarea1/src/logica/IControladorInsc.java
@@ -32,4 +32,5 @@ public interface IControladorInsc {
 	public abstract String[] listarPaquetesNoComprados();
 	public abstract void comprarPaquete(String nick, Date fecha, int cant, String paqString) throws PaqueteNoExisteException, PaqueteRepetidoException;
 	public abstract void inscribir(String nick, String nomSalida, int cantTuristas, Date fecha, String nombreAct, String nombrePaq) throws TuristaConSalida, ExcedeTuristas, InscFechaInconsistente, ActividadNoExisteException, InscFechaDespSalida, TuristaNoHaNacido, PaqueteRepetidoException, NoHayCuposException;
+	public abstract String[] obtenerPaquetesComprados(String nickTurista);
 }
