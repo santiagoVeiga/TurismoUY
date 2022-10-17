239b0551a0ef606db3821ac43ae44cabb0dfa985
Agustin <agustin.emanuel.de.leon@fing.edu.uy>
2022-10-17 17:35:26 -0300

    Arreglado tema seleccionar dptos y categorias

--- tarea2p2/src/main/java/Controllers/ServletConsulta.java
+++ tarea2p2/src/main/java/Controllers/ServletConsulta.java
@@ -34,85 +34,107 @@ public class ServletConsulta extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    public void selecDep(HttpServletRequest req, HttpServletResponse resp, String nomDpto) {
+        HttpSession session = req.getSession();
+        DataDepartamento[] aux = (DataDepartamento[]) session.getAttribute("dptos");
+        for(DataDepartamento it : aux) {
+            if(it.getNombre().equals(nomDpto)) {
+                session.setAttribute("DTDConsultaActividad", it);
+            }
+        }
+        try {
+            resp.sendRedirect("/tarea2p2/ConsultaActividad");
+        } catch (IOException e) {
+        }
+    }
+    
+    public void selecCat(HttpServletRequest req, HttpServletResponse resp, String nomCat) {
+        HttpSession session = req.getSession();
+        session.setAttribute("CatConsultaActividad", nomCat);
+        try {
+            resp.sendRedirect("/tarea2p2/ConsultaActividad");
+        } catch (IOException e) {
+        }
+    }
+    
 	private void processRequest(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		switch(req.getServletPath()){
-			case "/ConsultaUsuario":
-				String usuario = (String) req.getParameter("usuario");
-				conCons = fab.getIControladorConsulta();
-				if(usuario == null) {
-					DataUsuario[] usuarios = conCons.listarUsuarios();
-					req.setAttribute("ArregloUsuarios", usuarios);
-					req.getRequestDispatcher("/WEB-INF/ListaUsuario.jsp").forward(req,resp);
-				} else {
-					DataUsuario du = conCons.ingresarDatos(usuario);
-					req.setAttribute("UsuarioElegido", du);
-					req.getRequestDispatcher("/WEB-INF/ConsultaUsuario.jsp").forward(req,resp);
-				}				
-				break;
-			case "/ConsultaActividad":
-				HttpSession session = req.getSession();
-				String actividad = (String) req.getParameter("actividad");
-				DataActividad[] actividades = null;
-                String nomDpto = req.getParameter("DTDConsultaActividad");
-                DataDepartamento[] aux = (DataDepartamento[]) session.getAttribute("dptos");
-                if(nomDpto != null) {
-                    for(DataDepartamento it : aux) {
-                        if(it.getNombre().equals(nomDpto)) {
-                            actividades = (it).getColAct().toArray(new DataActividad[0]);
-                        }
-                    }
-                }
-                else if(session.getAttribute("DTDConsultaActividad")!= null) {
-					actividades = ((DataDepartamento) session.getAttribute("DTDConsultaActividad")).getColAct().toArray(new DataActividad[0]);
-				} else {
-					String categoria = (String) session.getAttribute("CatConsultaActividad");
-					if(categoria != null)
-					    actividades = conCons.obtenerActividadCategoria(categoria);
-				}
-				if(actividad == null) {
-					req.setAttribute("ArregloActividades", actividades);
-					req.getRequestDispatcher("/WEB-INF/ConsultaActividad/ListaActividad.jsp").forward(req,resp);
-				} else {
-				    int index = 0;
-					int i = 0;
-					// me daba error con While
-					for(i=0;i<actividades.length;i++) {
-					    if(actividades[i].getNombre()==actividad) {
-					        index=i;
-					    }
-					}
-					
-
-				    req.setAttribute("ActividadElegida", actividades[index]);
-                    req.getRequestDispatcher("/WEB-INF/ConsultaActividad/DetalleActividad.jsp").forward(req,resp);
-				}
-				break;
-			case "/ConsultaSalida":
-				req.getRequestDispatcher("/WEB-INF/ConsultaActividad.jsp").forward(req,resp); //Ver si entregar el set de salidas o no, por ahora se devuelve el DataSalida que viene desde la lista.
-				break;
-			case "/ConsultaPaquete":
-				String paquete = (String) req.getParameter("paquete");
-				if(paquete == null) {
-					conCons = fab.getIControladorConsulta();
-					String[] nombresPaq = conCons.listarPaquetes();
-					DataPaquete[] dps = new DataPaquete[nombresPaq.length];
-					for(int i=0;i<nombresPaq.length;i++) {
-						dps[i] = conCons.obtenerDataPaquete(nombresPaq[i]);
-					}
-					req.setAttribute("ArregloPaquetes", dps);
-					req.getRequestDispatcher("/WEB-INF/ConsultaPaquete/ListaPaquetes.jsp").forward(req,resp);
-				} else {
-					DataPaquete dp = conCons.obtenerDataPaquete(paquete);
-					req.setAttribute("PaqueteElegido", dp);
-					req.getRequestDispatcher("/WEB-INF/ConsultaPaquete/DetallePaquete.jsp").forward(req,resp);
-				}
-				break;
-			case "/DetallePaquete":
-				break;
-		}
+	    HttpSession session = req.getSession();
+        //Ver si se presiona sobre un dpto
+        String nomDpto = req.getParameter("DTDConsultaActividad");
+        String nomCat = req.getParameter("CatConsultaActividad");
+        if(nomDpto != null) {
+            selecDep(req,resp,nomDpto);
+        } else if (nomCat != null) {
+            selecCat(req,resp,nomCat);
+        } else {
+    		switch(req.getServletPath()){
+    			case "/ConsultaUsuario":
+    				String usuario = (String) req.getParameter("usuario");
+    				conCons = fab.getIControladorConsulta();
+    				if(usuario == null) {
+    					DataUsuario[] usuarios = conCons.listarUsuarios();
+    					req.setAttribute("ArregloUsuarios", usuarios);
+    					req.getRequestDispatcher("/WEB-INF/ListaUsuario.jsp").forward(req,resp);
+    				} else {
+    					DataUsuario du = conCons.ingresarDatos(usuario);
+    					req.setAttribute("UsuarioElegido", du);
+    					req.getRequestDispatcher("/WEB-INF/ConsultaUsuario.jsp").forward(req,resp);
+    				}				
+    				break;
+    			case "/ConsultaActividad":
+    				String actividad = (String) req.getParameter("actividad");
+    				DataActividad[] actividades = null;
+                    if(session.getAttribute("DTDConsultaActividad")!= null) {
+    					actividades = ((DataDepartamento) session.getAttribute("DTDConsultaActividad")).getColAct().toArray(new DataActividad[0]);
+    				} else {
+    					String categoria = (String) session.getAttribute("CatConsultaActividad");
+    					if(categoria != null)
+    					    actividades = conCons.obtenerActividadCategoria(categoria);
+    				}
+    				if(actividad == null) {
+    					req.setAttribute("ArregloActividades", actividades);
+    					req.getRequestDispatcher("/WEB-INF/ConsultaActividad/ListaActividad.jsp").forward(req,resp);
+    				} else {
+    				    int index = 0;
+    					int i = 0;
+    					// me daba error con While
+    					for(i=0;i<actividades.length;i++) {
+    					    if(actividades[i].getNombre()==actividad) {
+    					        index=i;
+    					    }
+    					}
+    					
+    
+    				    req.setAttribute("ActividadElegida", actividades[index]);
+                        req.getRequestDispatcher("/WEB-INF/ConsultaActividad/DetalleActividad.jsp").forward(req,resp);
+    				}
+    				break;
+    			case "/ConsultaSalida":
+    				req.getRequestDispatcher("/WEB-INF/ConsultaActividad.jsp").forward(req,resp); //Ver si entregar el set de salidas o no, por ahora se devuelve el DataSalida que viene desde la lista.
+    				break;
+    			case "/ConsultaPaquete":
+    				String paquete = (String) req.getParameter("paquete");
+    				if(paquete == null) {
+    					conCons = fab.getIControladorConsulta();
+    					String[] nombresPaq = conCons.listarPaquetes();
+    					DataPaquete[] dps = new DataPaquete[nombresPaq.length];
+    					for(int i=0;i<nombresPaq.length;i++) {
+    						dps[i] = conCons.obtenerDataPaquete(nombresPaq[i]);
+    					}
+    					req.setAttribute("ArregloPaquetes", dps);
+    					req.getRequestDispatcher("/WEB-INF/ConsultaPaquete/ListaPaquetes.jsp").forward(req,resp);
+    				} else {
+    					DataPaquete dp = conCons.obtenerDataPaquete(paquete);
+    					req.setAttribute("PaqueteElegido", dp);
+    					req.getRequestDispatcher("/WEB-INF/ConsultaPaquete/DetallePaquete.jsp").forward(req,resp);
+    				}
+    				break;
+    			case "/DetallePaquete":
+    				break;
+    		}
+        }
 	}
 	
 	/**
